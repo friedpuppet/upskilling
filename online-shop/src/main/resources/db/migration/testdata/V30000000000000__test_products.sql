b4f421a1a36327c7a1442c9851b0a056e23c32e7
Ievgen Yelisieiev <friedpuppet@pm.me>
2021-08-23 17:18:44 +0300

    Moved db config out of pom.xml Added some test data to flyway migration

--- online-shop/src/main/resources/db/migration/testdata/V30000000000000__test_products.sql
+++ online-shop/src/main/resources/db/migration/testdata/V30000000000000__test_products.sql
@@ -1,8 +1,8 @@
-insert into onlineshop.products (id, name, price, description, modify_date) values (1, 'eggs', 35, 'c0', PARSEDATETIME('2021-08-17 03:01:15', 'yyyy-MM-dd HH:mm:ss'));
-insert into onlineshop.products (id, name, price, description, modify_date) values (2, 'milk', 26.5, '3.2% fat', PARSEDATETIME('2021-08-19 13:47:42', 'yyyy-MM-dd HH:mm:ss'));
-insert into onlineshop.products (id, name, price, description, modify_date) values (3, 'milk', 22, '2.6% fat', PARSEDATETIME('2021-08-17 03:01:00', 'yyyy-MM-dd HH:mm:ss'));
-insert into onlineshop.products (id, name, price, description, modify_date) values (4, 'Dragon', 123123123, 'Small', PARSEDATETIME('2021-08-19 10:07:39', 'yyyy-MM-dd HH:mm:ss'));
-insert into onlineshop.products (id, name, price, description, modify_date) values (5, 'bike', 12312, 'another fat', PARSEDATETIME('2021-08-19 13:52:09', 'yyyy-MM-dd HH:mm:ss'));
-insert into onlineshop.products (id, name, price, description, modify_date) values (6, 'Pig', 2123.2, 'Huge one', PARSEDATETIME('2021-08-22 12:57:19', 'yyyy-MM-dd HH:mm:ss'));
-insert into onlineshop.products (id, name, price, description, modify_date) values (7, 'Lephant', 23346.34, 'Smally-wolly', PARSEDATETIME('2021-08-22 12:57:50', 'yyyy-MM-dd HH:mm:ss'));
+insert into onlineshop.products (id, name, price, description, modify_date) values (1, 'eggs', 35, 'c0', TO_TIMESTAMP('2021-08-17 03:01:15', 'YYYY-MM-DD HH24:MI:SS'));
+insert into onlineshop.products (id, name, price, description, modify_date) values (2, 'milk', 26.5, '3.2% fat', TO_TIMESTAMP('2021-08-19 13:47:42', 'YYYY-MM-DD HH24:MI:SS'));
+insert into onlineshop.products (id, name, price, description, modify_date) values (3, 'milk', 22, '2.6% fat', TO_TIMESTAMP('2021-08-17 03:01:00', 'YYYY-MM-DD HH24:MI:SS'));
+insert into onlineshop.products (id, name, price, description, modify_date) values (4, 'Dragon', 123123123, 'Small', TO_TIMESTAMP('2021-08-19 10:07:39', 'YYYY-MM-DD HH24:MI:SS'));
+insert into onlineshop.products (id, name, price, description, modify_date) values (5, 'bike', 12312, 'another fat', TO_TIMESTAMP('2021-08-19 13:52:09', 'YYYY-MM-DD HH24:MI:SS'));
+insert into onlineshop.products (id, name, price, description, modify_date) values (6, 'Pig', 2123.2, 'Huge one', TO_TIMESTAMP('2021-08-22 12:57:19', 'YYYY-MM-DD HH24:MI:SS'));
+insert into onlineshop.products (id, name, price, description, modify_date) values (7, 'Lephant', 23346.34, 'Smally-wolly', TO_TIMESTAMP('2021-08-22 12:57:50', 'YYYY-MM-DD HH24:MI:SS'));
 
